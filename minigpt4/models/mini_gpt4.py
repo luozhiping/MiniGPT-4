fa5b153160f48f76943ceffe761789abc17ed00b
peter <luozhiping.xidian@gmail.com>
2023-04-26 16:20:17 +0800

    minigpt

--- minigpt4/models/mini_gpt4.py
+++ minigpt4/models/mini_gpt4.py
@@ -150,13 +150,51 @@ class MiniGPT4(Blip2Base):
     def prompt_wrap(self, img_embeds, atts_img, prompt):
         if prompt:
             batch_size = img_embeds.shape[0]
-            p_before, p_after = prompt.split('<ImageHere>')
-            p_before_tokens = self.llama_tokenizer(
-                p_before, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
-            p_after_tokens = self.llama_tokenizer(
-                p_after, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
-            p_before_embeds = self.llama_model.model.embed_tokens(p_before_tokens.input_ids).expand(batch_size, -1, -1)
-            p_after_embeds = self.llama_model.model.embed_tokens(p_after_tokens.input_ids).expand(batch_size, -1, -1)
+            p_before_embeds = None
+            p_after_embeds = None
+            max_length = 0
+            for p in prompt:
+                max_length = max(max_length, len(p))
+            print(max_length)
+            for p in prompt:
+                p = p + ' ' * (max_length - len(p))
+                p_before, p_after = p.split('<ImageHere>')
+                print(p_before, p_after)
+                print("**")
+                print(self.llama_tokenizer(
+                    "</Img> Catch red block and put it next to blue block. ###Assistant:", return_tensors="pt", add_special_tokens=False).to(img_embeds.device).input_ids.shape)
+                print(self.llama_tokenizer(
+                    "</Img> Put red block to milk. ###Assistant:                        ", return_tensors="pt", add_special_tokens=False).to(img_embeds.device).input_ids.shape)
+                print(self.llama_tokenizer(
+                    "</Img> Put red block to milk. ###Assistant:########################", return_tensors="pt", add_special_tokens=False).to(img_embeds.device).input_ids.shape)
+                print(self.llama_tokenizer(
+                    " ", return_tensors="pt", add_special_tokens=False).to(img_embeds.device).input_ids)
+                print(self.llama_tokenizer("  ", return_tensors="pt", add_special_tokens=False).to(img_embeds.device).input_ids)
+                print(self.llama_tokenizer("   ", return_tensors="pt", add_special_tokens=False).to(img_embeds.device).input_ids)
+                print(self.llama_tokenizer("    ", return_tensors="pt", add_special_tokens=False).to(img_embeds.device).input_ids)
+                print("##")
+                p_before_tokens = self.llama_tokenizer(
+                    p_before, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
+                p_after_tokens = self.llama_tokenizer(
+                    p_after, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
+                if p_before_embeds is None:
+                    p_before_embeds = self.llama_model.model.embed_tokens(p_before_tokens.input_ids)
+                  #  print(p_before_embeds.shape, p_before_tokens.input_ids.shape)
+                 #   print(p_before_embeds)
+                else:
+                    tmp = self.llama_model.model.embed_tokens(p_before_tokens.input_ids)
+                #    print(p_before_embeds.shape, p_before_tokens.input_ids.shape, tmp.shape)
+                    p_before_embeds = torch.cat((p_before_embeds, tmp), dim=0)
+                if p_after_embeds is None:
+                    p_after_embeds = self.llama_model.model.embed_tokens(p_after_tokens.input_ids)
+                    print(p_after_embeds.shape, p_after_tokens.input_ids.shape)
+             #       print(p_after_embeds)
+                else:
+                    tmp = self.llama_model.model.embed_tokens(p_after_tokens.input_ids)
+               #     print(p_after_embeds.shape, p_after_tokens.input_ids.shape, tmp.shape)
+                    p_after_embeds = torch.cat((p_after_embeds, tmp), dim=0)
+            #print(p_before_embeds.shape)
+            #print(p_before_embeds)
             wrapped_img_embeds = torch.cat([p_before_embeds, img_embeds, p_after_embeds], dim=1)
             wrapped_atts_img = atts_img[:, :1].expand(-1, wrapped_img_embeds.shape[1])
             return wrapped_img_embeds, wrapped_atts_img
@@ -269,3 +307,4 @@ class MiniGPT4(Blip2Base):
             msg = model.load_state_dict(ckpt['model'], strict=False)
 
         return model
+
